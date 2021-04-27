886e1c51f2edadf8c1497a95baa9f38f8895a6ef
Pedro Henrique <pedrooplx@gmail.com>
2021-04-26 22:27:12 -0300

    ng g module

---
+++ spa/src/AppAngular/src/app/funcionalidade/funcionalidade.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    // componentes
+  ],
+  imports: [
+    // módulos
+    CommonModule
+  ],
+  providers: [
+    // serviços
+  ]
+})
+export class FuncionalidadeModule { }
