cb2c1febb5e2ff46ffdc71d901d79426a60c7fc0
Pedro Henrique <pedrooplx@gmail.com>
2021-04-26 22:18:03 -0300

    ng g c home

--- spa/src/AppAngular/src/app/app.module.ts
+++ spa/src/AppAngular/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TesteComponent } from './teste/teste.component';
-import { NomeComponenteComponent } from './pasta/nome-componente/nome-componente.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TesteComponent,
-    NomeComponenteComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
