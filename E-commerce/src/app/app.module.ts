79f75700a60cdd7ed447c4fa7107faff5dadf00d
Pedro Henrique <pedrooplx@gmail.com>
2021-04-27 20:08:50 -0300

    ultilizando pipes para titlecase e formatação de moeda

--- E-commerce/src/app/app.module.ts
+++ E-commerce/src/app/app.module.ts
@@ -4,6 +4,10 @@ import { APP_BASE_HREF } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from "@angular/common/http";
 
+import { registerLocaleData } from "@angular/common";
+import localPt from "@angular/common/locales/pt";
+registerLocaleData(localPt);
+
 import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
 import { HomeComponent } from './navegacao/home/home.component';
